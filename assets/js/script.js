3c622d1b1bca215ddbc8af2733c44c98e9b9ed98
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-01-16 22:49:24 -0500

    added dynamic elements with api functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+let latitude;
+let longitude;
+// open weather api key 
+var apiKey = "de4084fe21aab085a9b43a06f4ecd035";
+
 var formSubmitHandler = function(event) {
   // prevent page refresh after form submission
   event.preventDefault();
@@ -5,31 +10,31 @@ var formSubmitHandler = function(event) {
   var city = $("#searchterm").val().trim();
 
   if (city) {
-    getTodaysForecast(city);
-
+    getCoordinates(city);
     // clear form data
-    city = "";
+    $(city).text("");
+    // reset display
+    $("#forecasts").text("");
   } else {
     alert("Please enter a city!")
   }
 
 };
-
-var getTodaysForecast = function(city) {
-  // open weather api key 
-  var apiKey = "de4084fe21aab085a9b43a06f4ecd035";
+// grab location coordinates
+var getCoordinates = function(city) {
   // format the open weather api url
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
   
   // make a get request to apiUrl
   fetch(apiUrl)
   .then(function(response) {
     // if response was successful
     if (response.ok) {
-      console.log("the response was successful");
-
       response.json().then(function(data) {
-        console.log(data);
+        latitude = data.coord.lat;
+        longitude = data.coord.lon;
+
+        getForecast(latitude, longitude)
       });
     } else {
       alert("Error: " + response.statusText);
@@ -40,5 +45,165 @@ var getTodaysForecast = function(city) {
   })
 }
 
+// grab forecast information
+var getForecast = function (latitude, longitude) {
+  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude +"&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+
+  // make a get request to apiUrl
+  fetch(apiUrl)
+  .then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        var forecast = data;
+        displayTodaysForecast(forecast);
+        displayFiveDayForecast(forecast);
+      })
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to Open Weather. Try Again later.")
+  })
+}
+
+// function that displays the current day's forecast 
+var displayTodaysForecast = function(forecast) { 
+  // check if api returned any forecast
+  if (forecast.length === 0 ) {
+    $("#today").text() = "No forecast found";
+    return;
+  }
+
+  // create container 
+  var todayContainerEl = $("<article>")
+  .attr("id", "today")
+  .addClass("today border p-2")
+
+
+  // create header element
+  var cityName = $("#searchterm").val();
+  var headerEl = $("<h2>")
+  .addClass("font-weight-bold city-name-header text-capitalize")
+  .text(cityName)
+
+  // create temperature element
+  var temperature = forecast.current.temp;
+  var tempEl = $("<p>")
+  .addClass("temperature")
+  .text("Temp: " + temperature + "° F");
+
+
+  // create humidity p element
+  var humidity = forecast.current.humidity;
+  var humidityEl = $("<p>")
+  .addClass("humidity")
+  .text("Humidity: " + humidity + "%");
+
+  // create wind speed p element
+  var windSpeed = forecast.current.wind_speed;
+  var windSpeedEl = $("<p>")
+  .addClass("wind-speed")
+  .text("Wind: " + windSpeed + " MPH");
+
+  // create uv index span elements
+  var uvIndexLabel = $("<span>")
+  .text("UV Index: ");
+  var uvIndex = forecast.current.uvi;
+
+  if (uvIndex <= 2) {
+    // if uv index is favorable, change the span background to green
+    var uvIndexEl = $("<span>")
+    .addClass("uv-index p-1 rounded favorable")
+    .text(uvIndex);
+  } else if (uvIndex >= 3 && uvIndex <= 7) {
+    // if uv index is moderate, change the span background to yellow
+    var uvIndexEl = $("<span>")
+    .addClass("uv-index p-1 rounded moderate")
+    .text(uvIndex);
+  } else if (uvIndex >= 8) {
+    // if uv index is severe, change the span background to red
+    var uvIndexEl = $("<span>")
+    .addClass("uv-index p-1 rounded severe")
+    .text(uvIndex);
+  }
+
+  // append elements
+  $("#forecasts").append(todayContainerEl);
+  $(todayContainerEl).append(headerEl, tempEl, humidityEl, windSpeedEl, uvIndexLabel, uvIndexEl);
+};
+
+// function that displays the five-day forecast 
+var displayFiveDayForecast = function(forecast) {
+  // create container
+  var forecastContainerEl = $("<article>")
+  .attr("id", "five-day")
+  .addClass("five-day-container row d-flex justify-content-around mb-3 mt-3")
+  var forecastHeaderEl = $("<h3>")
+  .addClass("font-weight-bold")
+
+  // for loop that creates elements for the upcoming five day forecast
+  for (i = 0; i < 5; i++) {
+    var temp = forecast.daily[i].temp.day;
+    var wind = forecast.daily[i].wind_speed;
+    var humidity = forecast.daily[i].humidity;
+    var icon = forecast.daily[i].weather[0].icon;
+    var iconSrc = "http://openweathermap.org/img/w/" + icon + ".png";
+
+    // create the card for each day
+    var dayCard = $("<div>")
+    .addClass("card text-left p-3 mr-3")
+
+
+    var iconEl = $("<div>")
+    .attr("id", "icon-container")
+
+    var imgEl = $("<img>")
+    .attr("id", "icon")
+    .attr("src", iconSrc)
+    
+    var tempEl = $("<p>")
+    .addClass("forecast-temp")
+    .text("Temp: " + temp + "° F")
+
+    var windEl = $("<p>")
+    .addClass("forecast-wind")
+    .text("Wind: " + wind + " MPH");
+
+    var humidityEl = $("<p>")
+    .addClass("forecast-humidity")
+    .text("Humidity: " + humidity + "%")
+
+    //append to the card
+    $(iconEl).append(imgEl);
+    $(dayCard).append(iconEl, tempEl, windEl, humidityEl);
+    // append to the container
+    $(forecastContainerEl).append(dayCard)
+  }
+  // append elements
+  $("#forecasts").append(forecastContainerEl)
+};
+
+// function that creates search history button elements
+var createSearchHistoryButton = function (event) {
+  // create button
+  var searchTerm = $("#searchterm").val();
+  var historyButton = $("<button>")
+  .attr("type", "button")
+  .addClass("history-button btn btn-secondary mb-1 text-capitalize")
+  .text(searchTerm);
+
+  //append to the search History
+  $("#search-history").append(historyButton)
+}
+
+
+
 // event handlers
 $("#search-form").on("submit", formSubmitHandler);
+
+$("#search-form").on("submit", createSearchHistoryButton);
+
+$("#search-history").on("click", "button", function(keyword) {
+  console.log($(this))
+})
